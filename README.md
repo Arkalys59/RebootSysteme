e3b8a4b94e34d3d5d1b6826364e1a6b7a7030847
natrasta2_Dev/FoxShield <53398482+natrasta2dev@users.noreply.github.com>
2023-02-27 09:58:21 +0100

    Summary

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RebootSysteme
 Annonce de reboot serveur 
+
+
+[preview] (https://www.youtube.com/watch?v=BUhvPNtUbXE)
+Pour plus de script direction [discord] (https://discord.gg/PDC8m8gan4)
---
+++ reboot/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+
+ui_page('client/html/index.html')
+files({
+    'client/html/index.html',
+    'client/html/sounds/alarm.ogg'
+})
